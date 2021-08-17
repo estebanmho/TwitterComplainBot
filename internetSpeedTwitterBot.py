23a8149d2ba157433d866cf378c1d753c5b9c7bc
estebanmho <esteban.martinez.hoces@alumnos.upm.es>
2021-08-17 14:29:22 +0200

    Finished Project

--- internetSpeedTwitterBot.py
+++ internetSpeedTwitterBot.py
@@ -1,7 +1,13 @@
+import os
+
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
+
+PASSWORD = os.environ.get("PASSWORD")
+EMAIL = os.environ.get("EMAIL")
+
 
 class InternetSpeedTwitterBot:
 
@@ -25,7 +31,7 @@ class InternetSpeedTwitterBot:
         start_button = self.driver.find_element_by_class_name("start-text")
         start_button.click()
 
-        time.sleep(120)
+        time.sleep(90)
         try:
             down_speed = self.driver.find_element_by_class_name("download-speed").text
             self.down = float(down_speed)
@@ -36,23 +42,32 @@ class InternetSpeedTwitterBot:
             print(self.up)
 
         except:
-            print("There was not enough time to finish the test")
-        finally:
             self.driver.quit()
+            print("There was not enough time to finish the test")
+        else:
+            if self.down < 500:
+                self.tweet_at_provider()
+            else:
+                self.driver.quit()
 
     def tweet_at_provider(self):
-        self.driver.get()
-
-        log_in = self.driver.find_element_by_xpath("//*[@id=\"react-root\"]/div/div/div/main/div/div/div[1]/div[1]/div/div[3]/div[4]/span/span")
-        log_in.click()
-
-        google_log_in = self.driver.find_element_by_xpath("//*[@id=\"react-root\"]/div/div/div/main/div/div/div[1]/div[1]/div/div[3]/div[1]/div/span/span")
-        google_log_in.click()
-
+        self.driver.get("https://twitter.com/login")
         time.sleep(2)
+        email = self.driver.find_element_by_name("session[username_or_email]")
+        email.send_keys(EMAIL)
 
-        self.driver.switch_to.window(self.driver.window_handles[1])
+        password = self.driver.find_element_by_name("session[password]")
+        password.send_keys(PASSWORD)
 
-        email = self.driver.find_element_by_id("identifierId")
-        email.send_keys()
+        login = self.driver.find_element_by_xpath("//*[@id=\"react-root\"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div/span/span")
+        login.click()
 
+        time.sleep(5)
+
+        tweet = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div/div/div/div/span')
+        tweet.send_keys(f"Hey internet provider, why is my internet speed {self.down}down/{self.up}up when I pay for 50/50")
+
+        tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div/div/span/span')
+        tweet_button.click()
+        time.sleep(2)
+        self.driver.quit()
